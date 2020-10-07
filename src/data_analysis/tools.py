caad23bb18f77d69fb3f33dc8877c6950b477591
Beatriz Medeiros <bmedeir1@jhu.edu>
2020-10-07 11:16:15 -0400

    datasets

--- src/data_analysis/tools.py
+++ src/data_analysis/tools.py
@@ -2,7 +2,7 @@ import numpy as np
 from numpy.linalg import eig
 import pandas as pd
 
-def ac_pca(X, Y):
+def ac_pca(X, Y, lam):
     X_rows, X_cols = X.shape
 
     column_names = ['PC' + str(i) for i in range(1, X_cols + 1)]
@@ -14,9 +14,14 @@ def ac_pca(X, Y):
     Y = Y - Y_mean
 
     # AC-PCA
-    lam = 20  # lambda
+#     lam = 20  # lambda
+#     lam = 100
     K = Y @ Y.T
-    cov = X.T @ X - lam * X.T @ K @ X
+    cov_1 = X.T @ X 
+    cov_2 = lam * X.T @ K @ X
+#     print('cov_1:\n', cov_1)
+#     print('cov_2:\n', cov_2)
+    cov = cov_1 - cov_2
     eigenvals, eigenvecs = eig(cov.T)
     P = eigenvecs.T.dot(X.T)
     P = P.T
